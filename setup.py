47eb22336b623fa7f3594da0fb4c59dfaac40c19
David Del Real <warhead_@hotmail.com>
2018-09-24 09:51:27 -0500

    Solved minor proble with danbooru link generation

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='hscraper',
-    version='0.4',
+    version='0.5',
     description='A tool to rip galleries from ehentai, r34.xxx, danbooru.donmai and hitomi.la',
     url='https://github.com/XrossFox/hscraper',
     author='XrossFox',
