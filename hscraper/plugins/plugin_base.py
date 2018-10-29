c8380cfec7ca67bb638e17492a52984a6b66e19d
David Del Real <warhead_@hotmail.com>
2018-10-29 15:40:20 -0600

    Modified tests so they suit the ABC.

--- hscraper/plugins/plugin_base.py
+++ hscraper/plugins/plugin_base.py
@@ -1,34 +1,82 @@
 import requests
 from fake_useragent import UserAgent
 from time import sleep
+from abc import ABC, abstractmethod
 
-class PluginBase(object):
+
+class PluginBase(ABC):
     '''
     Base class for scraper plug-ins.
     '''
-
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
+    @abstractmethod
+    def start(self, url, pages, skip_pages, wait, retries, wait_retries, output):
+        """
+        Abstract method for control flow
+        """
+        pass
     
-    def get_html(self, url, wait, retry, wait_retry):
+    @abstractmethod
+    def scrap_for_pages(self):
+        """
+        Abstract method that scraps html code for pages
+        """
+        pass
+    
+    @abstractmethod
+    def scrap_for_posts(self):
+        """
+        Abstract method that scraps html code for posts
+        """
+        pass
+    
+    @abstractmethod
+    def scrap_for_images(self):
+        """
+        Abstract method that scraps html code for images
+        """
+        pass
+    
+    def create_dir(self):
+        """
+        Creates output directory to store downloaded galleries
+        """
+        pass
+    
+    def write_to(self):
+        """
+        Writes images to directory
+        """
+        pass
+    
+    def get_request(self, url, wait, retry, wait_retry, **cookies):
+        """
+        Sends an http request to the given url. returns a dictionary with 3 keys:
+        
+        payload: the response
+        responce_code: self explanatory
+        retry: how many tries were done
+        
+        You can set number of retries and time between retries with given parameters.
+        
+        You can also send a dictionary as cookies.
+        """
+        
         ua = UserAgent(cache=False)
         headers = {"User-Agent" : ua.random}
+        cookies = cookies
         res = {'retry':retry, 'response_code':404, 'payload':None}
         
         for n_retry in range(retry):
             try:
                 
-                req = requests.get(url, headers=headers)
+                req = requests.get(url, headers=headers, cookies=cookies)
                 res['payload'] = req.text
                 res['response_code'] = req.status_code
                 res['retry'] = n_retry + 1
                 break
                 
             except Exception as w:
-
+                
                 res['payload'] = None
                 res['response_code'] = 404
                 res['retry'] = n_retry + 1 
@@ -36,5 +84,3 @@ class PluginBase(object):
                 
         sleep(wait)
         return res
-    
-        
--- tests/plugin/test_plugin_base.py
+++ tests/plugin/test_plugin_base.py
@@ -3,22 +3,37 @@ sys.path.append('../../hscraper/plugin')
 
 import unittest
 from bs4 import BeautifulSoup
+import hashlib
 
 import plugin_base
+
+class TestClass(plugin_base.PluginBase):
+    
+    def start(self):
+        pass
+    
+    def scrap_for_images(self):
+        pass
+    
+    def scrap_for_pages(self):
+        pass
+    
+    def scrap_for_posts(self):
+        pass
 
 class Test(unittest.TestCase):
     """
     Test Class for plugin base. It tests the methods that do not need to be overriden across plugins.
     """
     def setUp(self):
-        self.pb = plugin_base.PluginBase()
+        self.pb = TestClass()
 
     def test_get_html(self):
         """
         Tests the request for an html page
         """
         
-        response = self.pb.get_html("https://www.webscraper.io/test-sites/tables",1 ,3, 1)
+        response = self.pb.get_request("https://www.webscraper.io/test-sites/tables",1 ,3, 1)
         parser = BeautifulSoup(response["payload"], "html.parser")
         
         tag = parser.find("h1")
@@ -35,7 +50,7 @@ class Test(unittest.TestCase):
         Tests the request for an html page, given a bad url.
         """
         
-        response = self.pb.get_html("https://www.websc/es/taasdasd",1 ,1 ,1)        
+        response = self.pb.get_request("https://www.websc/es/taasdasd",1 ,1 ,1)        
         
         self.assertEqual(response["payload"], None)
         self.assertEqual(response["retry"], 1)
@@ -46,15 +61,13 @@ class Test(unittest.TestCase):
         Tests the request for an html page, given a bad url, must retry 3 times.
         """
         
-        response = self.pb.get_html("https://www.websc/es/taasdasd", 1, 3, 1)
+        response = self.pb.get_request("https://www.websc/es/taasdasd", 1, 3, 1)
         
         self.assertEqual(response["retry"], 3)
         self.assertEqual(response["response_code"], 404)
         self.assertEqual(response["payload"], None)
         
-    def _get_img(self):
-        pass
-
+    
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
