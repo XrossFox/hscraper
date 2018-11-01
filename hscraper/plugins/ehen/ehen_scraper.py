ad92145b6c9d110338960140b4fb8fb56d38465e
David Del Real <warhead_@hotmail.com>
2018-11-01 15:05:49 -0600

    Added methods and tests for: validate_url, scrap_for_pages, and scrap_for_posts.
     Changes to be committed:
    	modified:   hscraper/plugins/ehen/ehen_scraper.py
    	modified:   tests/plugins/ehen/test_ehen.py

--- hscraper/plugins/ehen/ehen_scraper.py
+++ hscraper/plugins/ehen/ehen_scraper.py
@@ -1,4 +1,6 @@
 import plugin_base
+import re
+from bs4 import BeautifulSoup
 
 
 class EhenScraper(plugin_base.PluginBase):
@@ -16,19 +18,54 @@ class EhenScraper(plugin_base.PluginBase):
         pass
     
     def validate_url(self,url):
-        pass
+        """
+        Validates URL using a regular expression.
+        """
+        if re.match(r"https:\/\/e-hentai\.org\/g\/[\d]+\/[\S]{10}[\/\?p=\d+]+", url):
+            return True;
+        return False
     
-    def scrap_for_pages(self):
+    def scrap_for_pages(self, url, pages, skip_from=None, skip_to=None):
         """
-        Abstract method that scraps html code for pages
+        Returns a list of urls for each page given an ehentai url.
+        Doesn't really scrap anything. You can set a range to skip certain pages (exclusive in both ends).
+        If skip_from is set, it will skip pages until the end, or up to skip_to.
+        To use skip_to, skip_from must be set first.
         """
-        pass
+        #request data for retrieval
+        url_lists = list()
+        
+        if not skip_to:
+            skip_to = pages
+            
+        if not skip_from:
+            skip_from = 0
+        
+        for page in range(skip_from + 1, skip_to+1):
+            if page > 1:
+                url_lists.append(url+'?p='+str(page-1))
+            else:
+                url_lists.append(url)
+        return url_lists
     
-    def scrap_for_posts(self):
+    def scrap_for_posts(self, url, wait, retry, wait_retry):
         """
-        Abstract method that scraps html code for posts
+        Scraps all post url in a given page. Returns a list of links to each post.
+        If it receives an invalid url or None, it returns None.
         """
-        pass
+        if url == None:
+            return None
+        
+        response = self.get_request(url, wait, retry, wait_retry)
+        
+        if response['response_code'] != 200:
+            return None
+
+        soup = BeautifulSoup(response['payload'],"html.parser")
+        div = soup.find(id="gdt")
+        tags = div.find_all("a")
+        links = [a.get("href") for a in tags]
+        return links
     
     def scrap_for_images(self):
         """
--- tests/plugins/ehen/test_ehen.py
+++ tests/plugins/ehen/test_ehen.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append('../../../hscraper/plugins')
 sys.path.append('../../../hscraper/plugins/ehen')
 
+import re
+
 import unittest
 import ehen_scraper
 
@@ -12,16 +14,82 @@ class Test(unittest.TestCase):
         self.ehen = ehen_scraper.EhenScraper()
 
     def test_scrap_for_pages(self):
+        """
+        Tests if it can generate the urls of the pages given a starting url
+        """
+        
         given_url = "https://e-hentai.org/g/1087428/b240f1a9ab/"
         
-        expecter_urls = ["https://e-hentai.org/g/1087428/b240f1a9ab/",
+        expected_urls = ["https://e-hentai.org/g/1087428/b240f1a9ab/",
                          "https://e-hentai.org/g/1087428/b240f1a9ab/?p=1",
                          "https://e-hentai.org/g/1087428/b240f1a9ab/?p=2",
                          "https://e-hentai.org/g/1087428/b240f1a9ab/?p=3",
                          "https://e-hentai.org/g/1087428/b240f1a9ab/?p=4",]
         
-        url_lists = self.ehen.scrap_for_pages()
-
+        url_lists = self.ehen.scrap_for_pages(given_url, 5, None, None)
+        
+        for i in range(len(expected_urls)):
+            self.assertEqual(url_lists[i], expected_urls[i])
+            
+    def test_scrap_for_pages_skips_from_2(self):
+        """
+        Tests if skips pages 1 and 2 from 1 to 5
+        """
+        given_url = "https://e-hentai.org/g/1087428/b240f1a9ab/"
+        
+        expected_ouput = ["https://e-hentai.org/g/1087428/b240f1a9ab/?p=2",
+                         "https://e-hentai.org/g/1087428/b240f1a9ab/?p=3",
+                         "https://e-hentai.org/g/1087428/b240f1a9ab/?p=4",]
+        
+        out = self.ehen.scrap_for_pages(given_url, 5, skip_from=2)
+        for i in range(3):
+            self.assertEqual(out[i], expected_ouput[i])
+            
+    def test_scrap_for_pages_skip_from_2_to_3(self):
+        """
+        Tests if skips pages 1 and 2 to 5, from 1 to 5
+        """
+        given_url = "https://e-hentai.org/g/1087428/b240f1a9ab/"
+        
+        expected_ouput = ["https://e-hentai.org/g/1087428/b240f1a9ab/?p=1",
+                         "https://e-hentai.org/g/1087428/b240f1a9ab/?p=2",]
+            
+        out = self.ehen.scrap_for_pages(given_url, 5, skip_from=1, skip_to=3)
+        for i in range(2):
+            self.assertEqual(out[i], expected_ouput[i])
+            
+    def test_validate_url(self):
+        """
+        Tests if it validates urls using a regular expression
+        """
+        
+        given_urls = ["https://e-hentai.org/g/1087428/b240f1a9ab/",
+                      "https://e-hentai.org/g/1087428/b240f1a9ab/?p=1",
+                      "https://e-hentai.org/g/1087428/b240f1a9ab/?p=2",
+                      "https://e-hentai.org/g/1087428/b240f1a9ab/?p=3",
+                      "https://e-hentai.org/g/1087428/b240f1a9ab/?p=4",
+                      ]
+        bad_url = "wrong!"
+        
+        for url in given_urls:
+            self.assertTrue(self.ehen.validate_url(url))
+        
+        self.assertFalse(self.ehen.validate_url(bad_url))
+        
+    def test_scrap_for_posts(self):
+        """
+        Tests scrap_for_posts returns valid post urls using a regular expression
+        """
+        
+        url = "https://e-hentai.org/g/1087428/b240f1a9ab/"
+        
+        response = self.ehen.scrap_for_posts(url, 1, 1, 1)
+        
+        pat = re.compile(r'https:\/\/e-hentai\.org\/s\/[\S]{10}\/[\d]+-[\d]+')
+        for post in response:
+            self.assertTrue(pat.match(post))
+        
+    
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
