2c28d01e45408e43b44ae2e83e8afaa75b94616c
David Del Real <warhead_@hotmail.com>
2018-11-01 14:15:34 -0600

    first test

--- tests/plugins/ehen/test_ehen.py
+++ tests/plugins/ehen/test_ehen.py
@@ -11,8 +11,16 @@ class Test(unittest.TestCase):
     def setUp(self):
         self.ehen = ehen_scraper.EhenScraper()
 
-    def testName(self):
-        pass
+    def test_scrap_for_pages(self):
+        given_url = "https://e-hentai.org/g/1087428/b240f1a9ab/"
+        
+        expecter_urls = ["https://e-hentai.org/g/1087428/b240f1a9ab/",
+                         "https://e-hentai.org/g/1087428/b240f1a9ab/?p=1",
+                         "https://e-hentai.org/g/1087428/b240f1a9ab/?p=2",
+                         "https://e-hentai.org/g/1087428/b240f1a9ab/?p=3",
+                         "https://e-hentai.org/g/1087428/b240f1a9ab/?p=4",]
+        
+        url_lists = self.ehen.scrap_for_pages()
 
 
 if __name__ == "__main__":
