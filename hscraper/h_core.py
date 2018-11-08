3a138088e7cdb107bea85b75046863fdcaff7cd1
David Del Real <warhead_1090@hotmail.com>
2018-11-08 16:52:46 -0600

    Updated docs in h_core and removed debug prints

--- hscraper/h_core.py
+++ hscraper/h_core.py
@@ -33,26 +33,19 @@ class HCore():
             self.hit.start(url=url, from_img=skip_from, to_img=pages, wait=wait, retry=retry, wait_retry=wait_retry, output=output)
                 
 @click.command()
-@click.option('-b',help="Path to a txt with multiple urls as: url,pages. One per line."+
-              " This option overrides -u, and -p (-t and -f down work with -b yet). Throw it (the path to txt) between quotes for safe measure.")
-@click.option('-u',help="Gallery (or reader) url, throw it between quotes for safe measure.")
-@click.option('-f',help="Skip from page given.",default=None)
+@click.option('-b',help="(Absolute) Path to a txt with multiple urls as: url,page[,skip_from]. One per line."+
+              " This option overrides -u, -p and -f. Throw it (the path to txt) between quotes for safe measure.")
+@click.option('-u',help="Gallery url, throw it between quotes for safe measure.")
+@click.option('-f',help="Skip from page -f to page -p.",default=None)
 @click.option('-p',help="Pages to scrap. Default is 1",default=1)
 @click.option("-o",help="Output directory, throw it between quotes for safe measure.")
-@click.option('-w',help="Wait time between downloads, defaut is 1.0 secobds.",default=1.0)
+@click.option('-w',help="Wait time between http requests, defaut is 3.0 secobds.",default=3.0)
 @click.option('-r',help="Set number of retries. Default is 3.",default=3)
-@click.option('-x',help="Set wait time between retries. Default is 3.0 seconds.",default=3)
+@click.option('-x',help="Set wait time between retries. Default is 3.0 seconds.",default=3.0)
 def clickerino(b, u, f, p, o, w, r, x):
-    """Suported sites include: ehentai.org, r34.xxx,
-    danbooru.donmai and hitomi.la/reader.
-    \nWARNING: For hitomi.la, a page from the reader of the
-    desired gallery must be given, no need to set -p."""
+    """Scraps images from the following sites: danbooru.donmai, r34.xxx, ehentai.org and hitomi.la"""
     if f:
         f = int(f)
-    print("-b: {}:{}\n-u: {}:{}\n".format(type(b),b,type(u),u))
-    print("-p: {}:{}\n-f: {}:{}\n".format(type(p),p,type(f),f))
-    print("-o: {}:{}\n-w: {}:{}\n-r: {}:{}\n".format(type(o),o,type(w),w,type(r),r))
-    print("-x: {}:{}".format(type(x),x))
     if u is None and b is None:
         click.echo("Hey, there is no url, nor text file with links here!")
         click.echo("type --help to know more")
@@ -72,6 +65,9 @@ def clickerino(b, u, f, p, o, w, r, x):
             print(w)
 
 def batch_start(batch, wait, retry, wait_retry, output):
+    """
+    Calls core_start for each line in the text file.
+    """
     with open(batch) as bat:
         lines = bat.readlines()
     core = HCore()
